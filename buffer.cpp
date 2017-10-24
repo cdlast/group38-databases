ccdfaa6471baeea8b44c4ba355e6b2ac2316d4e9
cdlast <cdlast@wisc.edu>
2017-10-24 18:18:35 -0500

    Started readPage, allocPage, and disposePage
    They correspond with the methods in file.cpp

--- buffer.cpp
+++ buffer.cpp
@@ -16,7 +16,7 @@
 
 namespace badgerdb { 
 
-  // Constructor for BufMgr
+// Constructor for BufMgr
 BufMgr::BufMgr(std::uint32_t bufs)
 	: numBufs(bufs) {
 
@@ -36,63 +36,75 @@ BufMgr::BufMgr(std::uint32_t bufs)
   clockHand = bufs - 1;
 }
 
-  // Destructor for BufMgr
+// Destructor for BufMgr
 BufMgr::~BufMgr()
 {
   
 }
 
-  // Advances clock to next frame in the buffer pool
+// Advances clock to next frame in the buffer pool
 void BufMgr::advanceClock()
 {
-
+  // FrameId (uint32_t) clockHand
+  
+  // Check if clockHand has reached numBufs - 1
+  if (clockHand == numBufs - 1) {
+    clockHand = 0;
+  } else {
+    clockHand++;
+  }
 }
 
-  // Allocate a free frame
+// Allocate a free frame
 void BufMgr::allocBuf(FrameId & frame) 
 {
   
 }
 
-  // Reads the given page from the file into a frame and returns the pointer to page
-  // If the requested page is already present in the buffer pool,
-  //   pointer to that frame is returned
-  // Else a new frame is allocated from the buffer pool for reading the page
+// Reads the given page from the file into a frame and returns the pointer to page
+// If the requested page is already present in the buffer pool,
+//   pointer to that frame is returned
+// Else a new frame is allocated from the buffer pool for reading the page
 void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 {
-  
+  // Pointer might fudge this up
+  Page pageRead = file.readPage(pageNo, false); // second param prevents reading unused pages
+    
 }
 
-  // Unpin a page from memory since it is no longer required for it to remain in memory
+// Unpin a page from memory since it is no longer required for it to remain in memory
 void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
 {
   
 }
 
-  // Writes out all dirty pages of the file to disk
-  // All frames assigned to the file need to be unpinned from buffer pool before this works
-  // Else ERROR
+// Writes out all dirty pages of the file to disk
+// All frames assigned to the file need to be unpinned from buffer pool before this works
+// Else ERROR
 void BufMgr::flushFile(const File* file) 
 {
   
 }
 
-  // Allocates a new, empty page in the file and returns the Page object
-  // The new page is also assigned a frame in the buffer pool
+// Allocates a new, empty page in the file and returns the Page object
+// The new page is also assigned a frame in the buffer pool
 void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
 {
-  
+  // Gotta do some pointer BS with these
+  Page newPage = file.allocatePage();
+  PageId newPageNumber = newPage.page_number();
 }
 
-  // Delete a page from file and also from buffer pool if present
-  // Don't need to check if page is dirty
+// Delete a page from file and also from buffer pool if present
+// Don't need to check if page is dirty
 void BufMgr::disposePage(File* file, const PageId PageNo)
 {
-    
+  // must either use or be similar to these I think
+  file.deletePage(PageNo);
 }
 
-  // Print member variable values
-  // Don't change this
+// Print member variable values
+// Don't change this
 void BufMgr::printSelf(void) 
 {
   BufDesc* tmpbuf;
